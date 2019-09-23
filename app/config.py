b18b57551b819ee357955cdd4ab955089763ca81
lavylipesh <lilibethlangat@yahoo.com>
2019-09-23 16:23:34 -0400

    add email configurations

--- app/config.py
+++ app/config.py
@@ -4,7 +4,14 @@ class Config:
     SECRET_KEY='2wnd56mdj6hcmnc7cxn'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lilibeth:1234@localhost/quotes'
     SQLALCHEMY_TRACK_MODIFICATIONS = True 
-     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
 
