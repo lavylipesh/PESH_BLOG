ff4f1931ca6242adc980f01ad64d72652b9d08b5
lavylipesh <lilibethlangat@yahoo.com>
2019-09-23 16:34:47 -0400

    add function def register

--- app/auth/views.py
+++ app/auth/views.py
@@ -31,7 +31,7 @@ def register():
         user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
-        # mail_message("Welcome to PitchIt","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to PESH_BLOG","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
