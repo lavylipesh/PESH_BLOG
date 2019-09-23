52b3466d9617423fedef571d3dbec401c58563bb
lavylipesh <lilibethlangat@yahoo.com>
2019-09-23 16:38:06 -0400

    import datetime

--- app/models.py
+++ app/models.py
@@ -2,8 +2,7 @@ from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
-
-
+from datetime import datetime
 
 
 class User(UserMixin,db.Model):
