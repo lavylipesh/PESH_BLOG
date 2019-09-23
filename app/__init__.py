a13763410afceb362550d7b274be6e6dce874872
lavylipesh <lilibethlangat@yahoo.com>
2019-09-23 16:27:56 -0400

    import flask_mail

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
